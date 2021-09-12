4157f8ecdff96afc182ae66d6311e8e0ea31f921
RedLime <redlimerl@gmail.com>
2021-09-12 18:42:03 +0900

    Add api range

--- src/api/api.js
+++ src/api/api.js
@@ -8,12 +8,20 @@ export function setConnection(conn) {
 
 router.get('/responses/search/:type', async (req, res) => {
     const type = +req.params.type || 1000*60*5;
-    const [responses] = await connection.query(`SELECT MAX(res_time) as res_time, res_result, res_timestamp FROM responses WHERE req_type = 0 GROUP BY res_timestamp DIV ? ORDER BY res_timestamp DESC LIMIT 36`, [type]);
+    if (type > 1000*60*60*24*30 || type < 1000) {
+        res.json({error: "not allowed range"});
+        return;
+    }
+    const [responses] = await connection.query(`SELECT MAX(res_time) as res_time, res_result, res_timestamp FROM responses WHERE req_type = 0 GROUP BY res_timestamp DIV ? ORDER BY res_timestamp DESC LIMIT 36`, [Math.ceil(type)]);
     res.json(responses);
 });
 
 router.get('/responses/download/:type', async (req, res) => {
     const type = +req.params.type || 1000*60*5;
-    const [responses] = await connection.query(`SELECT MAX(res_time) as res_time, res_result, res_timestamp FROM responses WHERE req_type = 1 GROUP BY res_timestamp DIV ? ORDER BY res_timestamp DESC LIMIT 36`, [type]);
+    if (type > 1000*60*60*24*30 || type < 1000) {
+        res.json({error: "not allowed range"});
+        return;
+    }
+    const [responses] = await connection.query(`SELECT MAX(res_time) as res_time, res_result, res_timestamp FROM responses WHERE req_type = 1 GROUP BY res_timestamp DIV ? ORDER BY res_timestamp DESC LIMIT 36`, [Math.ceil(type)]);
     res.json(responses);
 });
